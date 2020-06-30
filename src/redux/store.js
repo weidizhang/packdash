0f76cd518d9d138206d8264b453d45b631bf10bf
Weidi Zhang <weidiz999@yahoo.com>
2020-06-29 21:34:21 -0700

    Use redux persist in order to save the states of saved packages; delete unused old files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import { applyMiddleware, createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
+
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-export default store;
+const persistConfig = {
+    key: "root",
+    storage: storage,
+    whitelist: ["savedCard"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
