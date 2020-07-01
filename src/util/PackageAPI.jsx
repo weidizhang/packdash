91a70b451fa0661d89b11cb2df9dd72cebf3facc
Weidi Zhang <weidiz999@yahoo.com>
2020-06-30 22:49:09 -0700

    Re-update api call from react side

--- src/util/PackageAPI.jsx
+++ src/util/PackageAPI.jsx
@@ -2,14 +2,12 @@ class PackageAPI
 {
     getTrackingData(tracking, carrier)
     {
-        const endpoint = "./carrier/" + carrier.toLowerCase();
+        // The backend point is /carrier/{carrier}/{tracking}
+        carrier = carrier.toLowerCase();
+        const endpoint = `./carrier/${carrier}/${tracking}`;
 
         // Return a promise to fetch the tracking data; it will be handled by the redux action
-        return fetch(endpoint, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ trackingNumber: tracking })
-        });
+        return fetch(endpoint, { method: "GET" });
     }
 }
 
