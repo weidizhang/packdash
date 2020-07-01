7991bb5a13919ebe4a05f569ed0fed26d200b41b
Weidi Zhang <weidiz999@yahoo.com>
2020-06-30 20:42:00 -0700

    Update api call from react side with the new changes

--- src/util/PackageAPI.jsx
+++ src/util/PackageAPI.jsx
@@ -2,11 +2,13 @@ class PackageAPI
 {
     getTrackingData(tracking, carrier)
     {
+        const endpoint = "./carrier/" + carrier.toLowerCase();
+
         // Return a promise to fetch the tracking data; it will be handled by the redux action
-        return fetch("./api/" + carrier, {
+        return fetch(endpoint, {
             method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: "trackingNum=" + tracking
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ trackingNumber: tracking })
         });
     }
 }
