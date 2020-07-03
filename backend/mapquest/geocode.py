6196b33dbc73cb0fc0a901b2fd1fc2792ece1335
Weidi Zhang <weidiz999@yahoo.com>
2020-07-03 14:00:59 -0700

    Fix issues with mapquest not fetching correct location by preprocessing location name

--- backend/mapquest/geocode.py
+++ backend/mapquest/geocode.py
@@ -1,10 +1,37 @@
 import json
 import mapquest.config as config
 import requests
+import us
 
 ENDPOINT = "http://open.mapquestapi.com/geocoding/v1/address"
+STATE_MAPPING = us.states.mapping("abbr", "name")
+
+def expand_location_name(location):
+    #! Mapquest API has issues with correctly determining location if the country
+    #! is not given in the location string and there is no zip code
+    #! 
+    #! Can account for this by converting states to their full name, i.e. CA -> California
+    location_parts = location.split()
+
+    for i in range(len(location_parts)):
+        expanded = False
+        part = location_parts[i]
+
+        # Only parts of the location with two letters can possibly be a state abbreviation
+        if len(part) == 2:
+            for abbr, full_name in STATE_MAPPING.items():
+                if abbr == part:
+                    location_parts[i] = full_name
+                    expanded = True
+                    break
+
+        if expanded:
+            break
+
+    return " ".join(location_parts)
 
 def location_to_latlng(location):
+    location = expand_location_name(location)
     response = requests.get(ENDPOINT, params = { "key": config.MAPQUEST_APP_KEY, "location": location })
     if not response:
         return False
